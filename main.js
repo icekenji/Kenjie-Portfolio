8e63409a0fee352e1b6538af31d2441f7af43a68
icekenji <kyleeeee2006@gmail.com>
2026-02-22 23:23:38 +0800

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-window.onload = () => {
-  document.body.classList.remove("container");
-
-  const music = document.getElementById("bg-music");
-
-  document.body.addEventListener("click", () => {
-    music.play();
-  }, { once: true });
-};
